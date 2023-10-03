c9a8def38aa20787d3f54976bb6389fdec73f2ae
Sabbir Ahmed <sabbir@yotech.ltd>
2023-10-03 11:55:59 +0600

    fix some improvements for readability

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
 
-const Accordion = ({items}) => {
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-    const [activeIndex,setActiveIndex] = useState(null)
- 
-     const onTitleClick = (index) =>{
-        setActiveIndex(index)
-     }
+  const onTitleClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
 
-    const renderedList = items.map((item,index)=>{
-        const active = index === activeIndex ? 'active':'';
-        return (
-               <React.Fragment key={item.title}>
-                 <div onClick={()=> onTitleClick(index)} className={`title ${active}`}>
-                  <i className="dropdown icon"></i>
-                  {item.title}
-                 </div>
-                 <div className={`content ${active}`}>
-                     <p>{item.content}</p> 
-                  </div>  
+  const renderedList = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
 
-                </React.Fragment>) 
-    })
     return (
-        <div className='ui styled accordion'>
-            {renderedList}
-                       
+      <React.Fragment key={item.title}>
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
     );
+  });
+
+  return <div className='ui styled accordion'>{renderedList}</div>;
 };
 
 export default Accordion;
--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,42 +1,48 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const Convert = ({text,language}) => {
-    const [translated,setTranslated] = useState('');
-    const [debouncedText,setDebouncedText] =useState(text);
+const Convert = ({ text, language }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
 
-    useEffect(()=>{
-      const timerId = setTimeout(() => {
-          setDebouncedText(text)
-      }, 500);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
 
-      return ()=>{
-          clearTimeout(timerId)
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      try {
+        const { data } = await axios.post(
+          'https://translation.googleapis.com/language/translate/v2',
+          {},
+          {
+            params: {
+              q: debouncedText,
+              target: language.value,
+              key: 'YOUR_GOOGLE_TRANSLATE_API_KEY', // Replace with your API key
+            },
+          }
+        );
+        setTranslated(data.data.translations[0].translatedText);
+      } catch (error) {
+        console.error('Error translating text:', error);
       }
-    },[text])
+    };
 
+    doTranslation();
+  }, [debouncedText, language]);
 
-    useEffect(()=>{
-
-     const doTranslation = async() =>{
-
-        const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-            params:{
-                q:debouncedText,
-                target:language.value,
-                key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }           
-          });
-         setTranslated(data.data.translations[0].translatedText) 
-     }
-    doTranslation()  
-       
-    },[debouncedText,language])
-    return (
-        <div>
-            <h3 className="ui header">{translated}</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="ui header">{translated}</h3>
+    </div>
+  );
 };
 
 export default Convert;
